b392c1368536739434f81cbddc9665605d1ef1bb
Roman Imankulov <roman.imankulov@gmail.com>
2019-11-01 22:37:35 +0000

    Refactor dash app

--- dash_app.py
+++ dash_app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+"""Dash application."""
 import datetime
 
 import dash
@@ -11,20 +12,52 @@ from dash.dependencies import Input, Output
 from wikiutils import aggregation_interval_sec, all_timestamps, redis_url
 
 r = redis.Redis.from_url(redis_url)
+app = dash.Dash(__name__)
 
 
-def get_layout():
-    return html.Div(
-        children=[
-            get_dropdown(),
-            dcc.Graph(id="event-rate-plot"),
-            dcc.Interval(
-                id="interval-component",
-                interval=aggregation_interval_sec * 1000,  # in milliseconds
-                n_intervals=0,
-            ),
-        ]
+def layout():
+    return html.Div(children=[dropdown(), graph(), interval()])
+
+
+def dropdown():
+    initial_value = ["en.wikipedia.org", "pt.wikipedia.org"]
+    domains = [d.decode("utf8") for d in r.smembers("known_domains")]
+    options = [{"value": d, "label": d} for d in domains]
+    return dcc.Dropdown(options=options, multi=True, id="domains", value=initial_value)
+
+
+def graph():
+    return dcc.Graph(id="event-rate-plot")
+
+
+def interval():
+    return dcc.Interval(
+        id="interval-component",
+        interval=aggregation_interval_sec * 1000,  # in milliseconds
+        n_intervals=0,
     )
+
+
+def get_events(domain):
+    redis_key = f"ev:{domain}"
+    timestamps = all_timestamps()
+    values = [int(v) if v else 0 for v in r.hmget(redis_key, timestamps)]
+    dt_list = [datetime.datetime.fromtimestamp(ts) for ts in timestamps]
+    return dt_list, values
+
+
+app.layout = layout()
+
+
+@app.callback(
+    Output(component_id="event-rate-plot", component_property="figure"),
+    [
+        Input(component_id="domains", component_property="value"),
+        Input(component_id="interval-component", component_property="n_intervals"),
+    ],
+)
+def update_output_div(domains, n):
+    return go.Figure(data=figure_data(domains), layout=figure_layout())
 
 
 def figure_data(domains):
@@ -36,39 +69,8 @@ def figure_data(domains):
     return ret
 
 
-def get_dropdown():
-    initial_value = ["en.wikipedia.org", "pt.wikipedia.org"]
-    domains = [d.decode("utf8") for d in r.smembers("known_domains")]
-    options = [{"value": d, "label": d} for d in domains]
-    return dcc.Dropdown(options=options, multi=True, id="domains", value=initial_value)
-
-
-def get_events(domain):
-    timestamps = all_timestamps()
-    redis_keys = [f"ev:{domain}:{ts}" for ts in timestamps]
-    values = [int(v) if v else 0 for v in r.mget(redis_keys)]
-    dt_list = [datetime.datetime.fromtimestamp(ts) for ts in timestamps]
-    return dt_list, values
-
-
-app = dash.Dash(__name__)
-app.layout = get_layout()
-
-
-@app.callback(
-    Output(component_id="event-rate-plot", component_property="figure"),
-    [
-        Input(component_id="domains", component_property="value"),
-        Input(component_id="interval-component", component_property="n_intervals"),
-    ],
-)
-def update_output_div(domains, n):
-    return go.Figure(
-        data=figure_data(domains),
-        layout=go.Layout(
-            title=f"Wikipedia changes per {aggregation_interval_sec} seconds"
-        ),
-    )
+def figure_layout():
+    return go.Layout(title=f"Wikipedia changes per {aggregation_interval_sec} seconds")
 
 
 if __name__ == "__main__":
